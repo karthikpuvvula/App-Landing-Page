19aa7d29e9ff618f2bf8429b775e58bd92682b65
Karthik Puvvula <karthikpuvvula@Karthiks-MacBook-Air.local>
2021-10-11 21:55:23 +0530

    Added App screenshots, pricing and contact us sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,6 +61,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // App Screen 
 
+var swiper = new Swiper('.swiper-container', {
+   effect: "coverflow",
+   grabCursor: true,
+   centeredSlides: true,
+   slidesPerView: "auto",
+   loop: true,
+   coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+   },
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+   }
+});
+
+
 
 
 // Side Navigation bar close while we click on navigation links
